f18fc918e06627388e9e525d9e6aceb3e34001ad
kemikal <janne@kemikal.org>
2019-09-20 10:20:29 +0200

    adds npm

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Circle cirlcle nothing to see here!");
+
+var sawtoothWave = new Pizzicato.Sound({ 
+    source: 'wave',
+    options: {
+        type: 'sine',
+        frequency: 440
+    }
+});
+
+var delay = new Pizzicato.Effects.Delay();
+sawtoothWave.addEffect(delay);
+
+sawtoothWave.play();
